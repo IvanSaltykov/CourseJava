b5e2820ad37fb8da2fd27fc7ab2e3a6bca4d5015
Ivan Saltykov <vano81003.is8@gmail.com>
2025-03-04 16:38:03 +0300

    4

--- Task 4/src/Main.java
+++ Task 4/src/Main.java
@@ -1,15 +1,11 @@
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.Scanner;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 public class Main {
     Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
-        //new Main().Number1();
-        //new Main().Number2();
+        new Main().Number1();
+        new Main().Number2();
         new Main().Number3();
     }
 
