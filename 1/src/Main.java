794a85d5c12a5f1fa0d2e960b03f3a4ff28fd13b
Ivan Saltykov <vano81003.is8@gmail.com>
2025-03-04 12:07:23 +0300

    1

--- 1/src/Main.java
+++ 1/src/Main.java
@@ -3,9 +3,9 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Как тебя зовут?");
+        System.out.println("РљР°Рє С‚РµР±СЏ Р·РѕРІСѓС‚?");
         Scanner scan = new Scanner(System.in);
         String myName = scan.nextLine();
-        System.out.println("Привет, " + myName);
+        System.out.println("РџСЂРёРІРµС‚, " + myName);
     }
 }
