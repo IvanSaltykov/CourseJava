19ae923825260cfd1ecc8842a9723bc43b641296
Ivan Saltykov <vano81003.is8@gmail.com>
2025-03-04 11:57:23 +0300

    1

--- 1/src/Main.java
+++ 1/src/Main.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Как тебя зовут?");
         Scanner scan = new Scanner(System.in);
         String myName = scan.nextLine();
