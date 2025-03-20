88a8324ed62d5fa50384b66841005e00312908b3
Ivan Saltykov <vano81003.is8@gmail.com>
2025-03-20 19:55:14 +0300

    .

--- String Boot/Task 2/src/Main.java
+++ String Boot/Task 2/src/Main.java
@@ -1,15 +1,23 @@
+import java.util.Scanner;
+import java.util.stream.LongStream;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите первое число: ");
+        int a = scanner.nextInt();
+        System.out.print("Введите второе число: ");
+        int b = scanner.nextInt();
+        if (a > b){
+            System.out.println("Первое число должно быть больше второго");
+            return;
         }
+        new Main().getArithmeticProgressionSum(a, b);
+    }
+    private void getArithmeticProgressionSum(int a, int b) {
+        long result = LongStream.rangeClosed(a, b).reduce(0, Long::sum);
+        System.out.println(result);
     }
 }
